22b1d6635b11dfcb60c4f4d3069c7029fd25db8e
erugani <enzorugan@gmail.com>
2024-03-12 15:09:32 -0300

    Teste de API - Post do CreateUser e GenerateToken

--- src/test/java/apitest/Account.java
+++ src/test/java/apitest/Account.java
@@ -3,6 +3,7 @@ package apitest;
 import com.google.gson.Gson;
 import entities.AccountEntity;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,25 +12,30 @@ import static org.hamcrest.Matchers.is;
 public class Account {
 
     String userId;
+    String ct = "application/json";
+    String jsonBody;
+    String uri = "https://bookstore.toolsqa.com/Account/v1/";
+    Response resposta;
+    String token;
     Gson gson = new Gson();
 
 
 
-    @Test
-    public void testCreateUser (){
+    @Test (priority = 1)
+        public void testCreateUser (){
         AccountEntity account = new AccountEntity();
-        account.userName = "charlie169";  //userID 2acb1634-452b-447d-a229-13aaeaf45539
+        account.userName = "charlie164";  //userID 2acb1634-452b-447d-a229-13aaeaf45539
         account.password = "P@ss0rd1";
 
-        String jsonBody = gson.toJson(account);
+        jsonBody = gson.toJson(account);
 
-        Response resposta = (Response) given()
-                .contentType("application/json")
+        resposta = (Response) given()
+                .contentType(ct)
                 .log().all()
                 .body(jsonBody)
 
         .when()
-                .post("https://bookstore.toolsqa.com/Account/v1/User")
+                .post(uri + "User")
 
         .then()
                 .log().all()
@@ -43,8 +49,30 @@ public class Account {
 
 
     }
-
+    @Test (priority = 2)
     public void testGenerateToken(){
+
+        resposta = (Response) given()
+                .contentType(ct)
+                .log().all()
+                .body(jsonBody)
+
+        .when()
+                .post(uri + "GenerateToken")
+
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("status", is ("Success"))
+                .body("result", is ("User authorized successfully."))
+
+        .extract()
+        ;
+
+        token  = resposta.jsonPath().getString("token");
+        System.out.println("token: " + token);
+
+        Assert.assertTrue(token.length() != 0);
 
     }
 
